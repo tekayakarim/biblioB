c7e876254588cef92d703c063d1a1787360c2d93
tekayakarim <abdelkarimbentekaya@gmail.com>
2022-07-22 00:17:06 +0200

    retierrdonetoo
    retirer book done too

--- app/controllers/userLivre.controller.js
+++ app/controllers/userLivre.controller.js
@@ -21,4 +21,23 @@ exports.ajouterLivresToUser=async (req, res, next) => {
     });
  
 };
+exports.retirerLivresToUser=async (req, res, next) => {
+    var livre;
+      Livre.findOne( req.libelleLivre ) .then((liv) => {
+          
+          console.warn(liv);
+          User.findOne( req.email ).then((user)=>{console.warn(user);
+              if(User.find(liv))
+              {
+                  user.livres.remove(liv);
+              User.updateOne(user)
+              .then((user) => res.status(201).json({ message: 'livre retiree!' }));
+              }
+              else {
+                res.status(201).json({ message: 'livre inexistant!' })
+              }
+          });
+      });
+   
+  };
 
--- app/routes/userLivre.routes.js
+++ app/routes/userLivre.routes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
 
 
 app.put('/ajouterLivreToUser',controller.ajouterLivresToUser);
+app.put('/retirerLivreToUser',controller.retirerLivresToUser);
 
 
 
